72cc423a506c66aa3e5556e111eccd7063929751
Pierre <jonkerpierre637@gmail.com>
2024-05-26 18:43:16 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,22 @@ admin.initializeApp({
 });
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = ['https://pierrejonker.github.io'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
 app.use(bodyParser.json());
 
 // Endpoint to check security question
