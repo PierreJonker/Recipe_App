e627e47e1ad15d01375795cad6af335954cf1abc
Pierre <jonkerpierre637@gmail.com>
2024-05-27 11:21:10 +0200

    Test 1

--- src/server.js
+++ src/server.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+// Initialize the app with a service account, granting admin privileges
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ['https://pierrejonker.github.io'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+app.use(bodyParser.json());
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello, world!' });
